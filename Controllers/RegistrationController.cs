3a5943e908a1353763d3579282a47661553e1a76
XunilTamtacles <bbondocmattlinux@gmail.com>
2026-04-06 19:50:09 +0800

    Registration not yet done

--- Controllers/RegistrationController.cs
+++ Controllers/RegistrationController.cs
@@ -7,11 +7,19 @@ namespace Lesson.Controllers
     [ApiController]
     public class RegistrationController : ControllerBase
     {
+        [HttpGet("v1/GetSampleData")]
         public IActionResult GetSampleData()
         {
             
             return Ok(new { Status = true, Guid = Guid.NewGuid() });
         }
 
+        [HttpGet("v2/GetSampleData")]
+        public IActionResult GetAnotherSampleData()
+        {
+            return Ok(new { Status = true, Guid = Guid.NewGuid(), Version = 2 });
+        }
+
+  
     }
 }
